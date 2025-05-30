b4d7fc9831ff1be56d5ff08bb3a3daa99435efb0
Ali Akram <aakram@Alis-MacBook-Air.local>
2025-05-30 20:21:07 +0100

    tables created

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # NC News Seeding
 
-- Instructions for this sprint can be found at https://l2c.northcoders.com/courses/be/seeding-nc-news
+# Instructions:
+- Two .env files are required in order to connect to the local PosgreSQL databases;
+1. .env.development
+2. .env.test 
+
+- You will be required to create both files in the root of the project directory. 
+
+Add the following configurations into the below files:
+
+# .env.development 
+PGDATABASE=nc_news
+
+# .env.test
+PGDATABASE=nc_news_test
+
+# gitignore 
+.env.*
+
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,61 @@
 const db = require("../connection")
 
-const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
+
+
+const seed = async ({ topicData, userData, articleData, commentData }) => {
+
+  await db.query('DROP TABLE IF EXISTS comments;');
+  await db.query('DROP TABLE IF EXISTS articles;');
+  await db.query('DROP TABLE IF EXISTS users;');
+  await db.query('DROP TABLE IF EXISTS topics;');
+
+
+  await db.query(`
+    CREATE TABLE topics (
+      slug VARCHAR(100) PRIMARY KEY NOT NULL,
+      description VARCHAR(1000),
+      img_url VARCHAR(1000)
+    );
+  `);
+
+  await db.query(`
+    CREATE TABLE users (
+      username VARCHAR(50) PRIMARY KEY NOT NULL,
+      name VARCHAR(50) NOT NULL,
+      avatar_url VARCHAR(1000)
+    );
+  `);
+
+  await db.query(`
+    CREATE TABLE articles (
+      article_id SERIAL PRIMARY KEY,
+      title VARCHAR(100) NOT NULL,
+      topic VARCHAR(100) NOT NULL,
+      author VARCHAR(50) NOT NULL,
+      body TEXT,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      votes INTEGER DEFAULT 0,
+      article_img_url VARCHAR(1000),
+      FOREIGN KEY (topic) REFERENCES topics(slug),
+      FOREIGN KEY (author) REFERENCES users(username)
+    );
+  `);
+
+  await db.query(`
+    CREATE TABLE comments (
+      comment_id SERIAL PRIMARY KEY,
+      article_id INTEGER NOT NULL,
+      body TEXT,
+      votes INTEGER DEFAULT 0,
+      author VARCHAR(30) NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY (article_id) REFERENCES articles(article_id),
+      FOREIGN KEY (author) REFERENCES users(username)
+    );
+  `);
+
 };
+
+
+
 module.exports = seed;
