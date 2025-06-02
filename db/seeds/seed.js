d9c472f04c088ceb94572da0e8c6a1945b4ef74a
Ali Akram <aakram@Alis-MacBook-Air.local>
2025-06-03 00:11:15 +0100

    data inserted

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,4 +1,5 @@
 const db = require("../connection")
+const { convertTimestampToDate } = require('./utils');
 
 
 
@@ -54,7 +55,84 @@ const seed = async ({ topicData, userData, articleData, commentData }) => {
     );
   `);
 
+
+  const newTopicData = topicData.map((topic) => {
+    return {
+      slug: topic.slug,
+      description: topic.description,
+      img_url: topic.img_url
+    }
+  });
+
+
+  for (const topic of newTopicData) {
+    await db.query(
+      'INSERT INTO topics (slug, description, img_url) VALUES ($1, $2, $3);',
+      [topic.slug, topic.description, topic.img_url]
+    );
+  }
+
+
+
+
+  const newUserData = userData.map((user) => {
+    return {
+      username: user.username,
+      name: user.name,
+      avatar_url: user.avatar_url
+    }
+  });
+
+  for (const user of newUserData) {
+    await db.query(
+      'INSERT INTO users (username, name, avatar_url) VALUES ($1, $2, $3);',
+      [user.username, user.name, user.avatar_url]
+    );
+  }
+
+  
+
+  const newArticleData = articleData.map((article) => {
+    return convertTimestampToDate ({
+      title: article.title,
+      topic: article.topic,
+      author: article.author,
+      body: article.body,
+      created_at: article.created_at,
+      votes: article.votes,
+      article_img_url: article.article_img_url
+    });
+  });
+
+  const articleKey = new Map();
+
+  for (const article of newArticleData) {
+    const values = await db.query(
+      'INSERT INTO articles (title, topic, author, body, created_at, votes, article_img_url) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING article_id, title;',
+      [article.title, article.topic, article.author, article.body, article.created_at, article.votes, article.article_img_url] );
+      articleKey.set(values.rows[0].title, values.rows[0].article_id);
+  }
+
+
+
+  const newCommentData = commentData.map((comment) => {
+    return convertTimestampToDate ({
+      article_id: articleKey.get(comment.article_title),
+      body: comment.body,
+      votes: comment.votes,
+      author: comment.author,
+      created_at: comment.created_at
+    });
+  });
+
+  for (const comment of newCommentData) {
+    const values = await db.query(
+      'INSERT INTO comments (article_id, body, votes, author, created_at) VALUES ($1, $2, $3, $4, $5);',
+      [comment.article_id, comment.body, comment.votes, comment.author, comment.created_at] );
+  }
+
 };
+
 
 
 
